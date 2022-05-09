e228d4dc6d2ee5a50c304a9e96764b96dcfe0dec
Nathan Evans <github@talkswithnumbers.com>
2022-05-08 20:35:48 -0700

    Clean up WordList

--- src/components/WordList/WordList.tsx
+++ src/components/WordList/WordList.tsx
@@ -1,8 +1,8 @@
+import type { IColumn } from '@fluentui/react';
 import { DetailsList } from '@fluentui/react'
 import { SelectionMode } from '@fluentui/utilities'
-import type { FC} from 'react';
-import { useCallback, useMemo , useState } from 'react'
-import styled from 'styled-components'
+import type { FC } from 'react'
+import { useCallback, useMemo } from 'react'
 
 export interface WordListProps {
 	words: string[]
@@ -16,36 +16,28 @@ export const WordList: FC<WordListProps> = ({ words, onClick }) => {
 			})),
 		[words],
 	)
+
+	const handleRowClick = useCallback(
+		(item: any) => onClick?.(item.word),
+		[onClick],
+	)
 	return (
-		<Container>
-			<DetailsList
-				compact
-				isHeaderVisible={false}
-				selectionMode={SelectionMode.single}
-				items={items}
-				columns={[
-					{
-						key: 'word',
-						name: 'Word',
-						fieldName: 'word',
-						minWidth: 100,
-					},
-				]}
-				onActiveItemChanged={item => onClick?.(item.word)}
-			/>
-		</Container>
+		<DetailsList
+			compact
+			isHeaderVisible={false}
+			selectionMode={SelectionMode.single}
+			items={items}
+			columns={columns}
+			onActiveItemChanged={handleRowClick}
+		/>
 	)
 }
 
-const Container = styled.div`
-	height: 100%;
-	width: 100%;
-	padding: 0;
-`
-
-const Word = styled.div<{ hovered: boolean }>`
-	cursor: pointer;
-	padding: 2px 2px 2px 8px;
-	background: ${({ hovered, theme }) =>
-		hovered ? theme.application().accent().hex() : 'none'};
-`
+const columns: IColumn[] = [
+	{
+		key: 'word',
+		name: 'Word',
+		fieldName: 'word',
+		minWidth: 100,
+	},
+]
